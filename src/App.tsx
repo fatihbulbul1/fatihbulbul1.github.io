30a7d41bbdb077f0aac7dab71564048a3fb7c6a0
fatihbulbul1 <fatihbulbul12@gmail.com>
2023-01-18 21:35:51 +0300

    mini commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { Projects } from "./assets/components/Projects";
 import { About } from "./assets/components/About";
 import { Footer } from "./assets/components/Footer";
 import { Navbar } from "./assets/components/Navbar";
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    fetch("https://frantic-duck-gear.cyclic.app/count");
+  }, []);
   return (
     <div className="App">
       <Navbar />
